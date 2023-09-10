373e9fa67ac21c35ca7d082b3f4f20aef39ca9ae
Huyền <94551970+huyenkun14@users.noreply.github.com>
2023-09-11 01:13:46 +0700

    fix add screen

--- front-end/src/screens/home/index.tsx
+++ front-end/src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View, Text, FlatList, Image } from 'react-native'
+import { ScrollView, View, Text, FlatList, Image, SafeAreaView } from 'react-native'
 import React from 'react'
 import { styles } from './styles'
 import Header from '../../components/header'
@@ -6,8 +6,41 @@ import { overview } from '../../mock/home'
 import Banner from './components/banner'
 import Entry from '../../components/entry'
 import { StatusBar } from 'expo-status-bar'
+import Option from './components/option'
 
 const Home = () => {
+  const optionsData = [
+    {
+      id: 1,
+      title: 'Quản lý',
+      icon: require('../../../assets/images/icon/ic_manager.png')
+    },
+    {
+      id: 2,
+      title: 'Tiết kiệm',
+      icon: require('../../../assets/images/icon/ic_saving.png')
+    },
+    {
+      id: 3,
+      title: 'Ngân sách',
+      icon: require('../../../assets/images/icon/ic_coins.png')
+    },
+    {
+      id: 4,
+      title: 'Lịch sử',
+      icon: require('../../../assets/images/icon/ic_history.png')
+    },
+    {
+      id: 5,
+      title: 'Cài đặt',
+      icon: require('../../../assets/images/icon/ic_setting.png')
+    },
+    {
+      id: 6,
+      title: 'Tài khoản',
+      icon: require('../../../assets/images/icon/ic_account.png')
+    }
+  ]
   const renderOverviewList = ({ item }) => (
     <View style={styles.overviewItem}>
       <View>
@@ -24,29 +57,19 @@ const Home = () => {
     </View>
   )
   return (
-    <View>
-      <StatusBar style='auto' />
+    <SafeAreaView>
+      <StatusBar style="auto" />
       <ScrollView style={styles.container}>
         {/* background */}
         <Image
           source={require('../../../assets/images/background1.png')}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            tintColor: '#EFF8FB'
-          }}
+          style={styles.imageBack}
         />
         {/* header */}
         <Header title='Trang chủ' isBack={false} />
-        {/* banner */}
-        <Banner />
+
         {/* overview */}
         <View style={styles.overviewContainer}>
-          <Text style={styles.title}>Tổng quan</Text>
-
           <FlatList
             horizontal
             showsHorizontalScrollIndicator={false}
@@ -55,15 +78,29 @@ const Home = () => {
             contentContainerStyle={{ paddingLeft: 25, paddingRight: 10 }}
           />
         </View>
+        {/* feature */}
+        <View style={styles.overviewContainer}>
+          <Text style={styles.title}>Danh mục</Text>
+          <View style={styles.optionContainer}>
+            {optionsData.map((item) => (
+              <Option title={item.title} icon={item.icon} key={item.id}/>
+            ))}
+            <View style={styles.emptyOption} />
+            <View style={styles.emptyOption} />
+          </View>
+        </View>
+        {/* banner */}
+        <Text style={styles.title}>Ads</Text>
+        <Banner />
         {/* last entries */}
         <View>
           <Text style={styles.title}>Gần đây</Text>
-          <Entry title='Food' time='02-09-2023' price={-200000} note='Ăn sáng' />
-          <Entry title='Bonus' time='01-09-2023' price={+500000} note='Thưởng lễ 2/9' />
-          <Entry title='Traffic' time='31-08-2023' price={-7000} note='Buýt' />
+          <Entry title='Food' time='02-09-2023' price={200000} note='Ăn sáng' status='chi tiêu' />
+          <Entry title='Bonus' time='01-09-2023' price={500000} note='Thưởng lễ 2/9' status='thu nhập' />
+          <Entry title='Traffic' time='31-08-2023' price={7000} note='Buýt' status='chi tiêu' />
         </View>
       </ScrollView>
-    </View>
+    </SafeAreaView>
   )
 }
 
