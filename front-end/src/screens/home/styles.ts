56162a22e1b44d466df530a82ed7670015054db7
Huyền <94551970+huyenkun14@users.noreply.github.com>
2023-09-13 22:44:41 +0700

    fix ui

--- front-end/src/screens/home/styles.ts
+++ front-end/src/screens/home/styles.ts
@@ -19,7 +19,7 @@ export const styles = StyleSheet.create({
     },
     title: {
         fontWeight: '300',
-        fontSize: 18,
+        fontSize: 16,
         marginBottom: 20,
         paddingHorizontal: 25,
     },
