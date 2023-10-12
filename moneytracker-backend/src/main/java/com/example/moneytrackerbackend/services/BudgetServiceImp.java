ebb1194fa0eea84b921ebaa869c9144a37d4e37a
camhh <camhh@hiveteach.vn>
2023-10-12 10:43:24 +0700

    budget change

--- moneytracker-backend/src/main/java/com/example/moneytrackerbackend/services/BudgetServiceImp.java
+++ moneytracker-backend/src/main/java/com/example/moneytrackerbackend/services/BudgetServiceImp.java
@@ -1,7 +1,20 @@
 package com.example.moneytrackerbackend.services;
 
+import com.example.moneytrackerbackend.entities.Budget;
+import com.example.moneytrackerbackend.entities.Category;
+import com.example.moneytrackerbackend.repositories.BudgetRepository;
+import com.example.moneytrackerbackend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BudgetServiceImp  implements BudgetService{
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private BudgetRepository budgetRepository;
+
+//    public Budget createBudget(){
+//
+//    }
 }
