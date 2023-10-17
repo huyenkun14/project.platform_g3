c705ee5c916bb39fdb50cf551e583b80bfcf1b32
camhh <camhh@hiveteach.vn>
2023-10-17 16:59:45 +0700

    Convert entity to response

--- moneytracker-backend/src/main/java/com/example/moneytrackerbackend/repositories/CategoryRepository.java
+++ moneytracker-backend/src/main/java/com/example/moneytrackerbackend/repositories/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.example.moneytrackerbackend.repositories;
 import com.example.moneytrackerbackend.entities.Category;
 import com.example.moneytrackerbackend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,8 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByTitle(String title);
-    Boolean existsByTitle(String title);
+    @Query("select COUNT(c) FROM Category c WHERE c.user.id = :userId AND c.title = :title")
+    int existsByTitle(@Param("title") String title, @Param("userId") Long userId);
     List<Category> findAllByValueAndUser(boolean value, User user);
     List<Category> findAllByUser(User user);
 
