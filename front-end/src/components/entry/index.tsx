ad0fe07c934f04b30b0da461706940efe7029cc4
Huyền <94551970+huyenkun14@users.noreply.github.com>
2023-11-07 22:20:51 +0700

    format new transaction

--- front-end/src/components/entry/index.tsx
+++ front-end/src/components/entry/index.tsx
@@ -14,25 +14,27 @@ const Entry = (props: any) => {
   const navigation = useNavigation<any>()
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(NAVIGATION_TITLE.DETAIL, { entryId })}>
-      <View style={styles.container}>
-        <View style={styles.typeContainer}>
-          <View style={styles.image}>
-            <Image style={styles.imageIcon} resizeMode='stretch' source={{ uri: imageUrl ? `${BASE_URL}${imageUrl}` : 'https://cdn-icons-png.flaticon.com/512/447/447120.png' }} />
-          </View>
-          <View>
-            <Text style={styles.title}>{title}</Text>
-            <Text numberOfLines={1} style={[styles.content, styles.note]}>{note}</Text>
-          </View>
+    <TouchableOpacity style={styles.container} onPress={() => navigation.navigate(NAVIGATION_TITLE.DETAIL, { entryId })}>
+      <View style={styles.typeContainer}>
+        {time === moment().format('YYYY-MM-DD') ?
+          <View style={styles.newIcon} />
+          : ''
+        }
+        <View style={styles.image}>
+          <Image style={styles.imageIcon} resizeMode='stretch' source={{ uri: imageUrl ? `${BASE_URL}${imageUrl}` : 'https://cdn-icons-png.flaticon.com/512/447/447120.png' }} />
         </View>
         <View>
-          {status ?
-            <Text style={[styles.title, styles.money, styles.alignRight]}>+ {formatMoneyWithVND(price)}</Text>
-            :
-            <Text style={[styles.title, styles.money, styles.alignRight]}>- {formatMoneyWithVND(price)}</Text>
-          }
-          <Text style={[styles.content, styles.alignRight]}>{moment(time).format('DD-MM-YYYY')}</Text>
+          <Text style={styles.title}>{title}</Text>
+          <Text numberOfLines={1} style={[styles.content, styles.note]}>{note}</Text>
         </View>
+      </View>
+      <View>
+        {status ?
+          <Text style={[styles.title, styles.money, styles.alignRight]}>+ {formatMoneyWithVND(price)}</Text>
+          :
+          <Text style={[styles.title, styles.money, styles.alignRight]}>- {formatMoneyWithVND(price)}</Text>
+        }
+        <Text style={[styles.content, styles.alignRight]}>{moment(time).format('DD-MM-YYYY')}</Text>
       </View>
     </TouchableOpacity>
   )
