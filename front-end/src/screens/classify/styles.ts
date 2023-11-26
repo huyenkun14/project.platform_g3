662c4aabbd52f58102b898832477bbcd1a0211f6
huyen khanh <khanhhuyen.tt@secomus.com>
2023-11-26 14:34:57 +0700

    fix option classify

--- front-end/src/screens/classify/styles.ts
+++ front-end/src/screens/classify/styles.ts
@@ -12,60 +12,35 @@ const styles = () => {
         option: {
             flexDirection: 'row',
             justifyContent: 'space-evenly',
-            alignItems:'center',
+            alignItems: 'center',
             paddingHorizontal: 25,
         },
         optionBtn: {
-            width: 100,
-            // backgroundColor: theme.flatListItem,
-            borderRadius: 8,
-            marginTop: 10,
-            marginBottom: 20,
-            paddingVertical: 10,
-            // shadowColor: theme.shadowColor,
-            // shadowOffset: {
-            //     width: 0,
-            //     height: 3,
-            // },
-            // shadowOpacity: 0.29,
-            // shadowRadius: 4.65,
-            // elevation: 7,
+            width: SCREEN_WIDTH / 2 - 25,
+            paddingBottom: 16,
+            borderBottomWidth: 2,
         },
         optionText: {
             textAlign: 'center',
             color: theme.text_white,
-            fontWeight: '500',
-            fontSize: 14,
+            fontWeight: '600',
+            fontSize: 16,
         },
-        searchContainer: {
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginHorizontal: 25,
-        },
-        searchView: {
-            height: 50,
-            width: SCREEN_WIDTH*0.6,
-            justifyContent: 'center',
-            borderRadius: 10,
-            borderColor: theme.borderColor,
-            borderWidth: 1,
-            paddingHorizontal: 10,
-        },
-        searchInput: {
-    
-        },
-        searchImageView: {
+        addImageView: {
             width: 50,
             marginTop: -10,
             backgroundColor: theme.flatListItem,
-            height: 40,
+            height: 50,
             justifyContent: 'center',
-            borderRadius: 10,
+            borderRadius: 50,
             alignItems: 'center',
+            position: 'absolute',
+            right: 25,
+            top: '30%'
         },
-        searchImage:{
-            height: 25,
-            width: 25,
+        addImage: {
+            height: 20,
+            width: 20,
             tintColor: theme.text_white,
             resizeMode: 'contain'
         }
